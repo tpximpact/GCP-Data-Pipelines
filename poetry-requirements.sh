cfb605cf265c4b2fd16bd84825abdaeff325926d
Balazs Roman <balazs.roman@tpximpact.com>
2024-03-07 18:07:18 +0000

    utility to help deployments

--- poetry-requirements.sh
+++ poetry-requirements.sh
@@ -3,7 +3,7 @@
 find . -not -path '*/\.*' -type f -name "*.toml" | while read toml_file; do
   dir=$(dirname "$toml_file")
   cd "$dir" || exit
-#   echo "Exporting requirements.txt in $dir"
+  echo "Exporting requirements.txt in $dir"
   poetry export --format=requirements.txt --output=requirements.txt --without-hashes
   cd - > /dev/null
 done
