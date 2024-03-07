22c7f2835eaaa7322835db62b752fe0e11e05f41
Balazs Roman <balazs.roman@tpximpact.com>
2024-03-07 15:42:31 +0000

    added employees pipe

---
+++ cloud_functions/hibob/employees/main.py
@@ -0,0 +1,49 @@
+import os
+
+import pandas as pd
+import requests
+
+from data_pipeline_tools.auth import hibob_headers
+from data_pipeline_tools.util import write_to_bigquery
+
+project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+if not project_id:
+    project_id = input("Enter GCP project ID: ")
+
+
+def load_config(project_id, service) -> dict:
+    return {
+        "table_name": os.environ.get("TABLE_NAME"),
+        "dataset_id": os.environ.get("DATASET_ID"),
+        "location": os.environ.get("TABLE_LOCATION"),
+        "headers": hibob_headers(project_id, service),
+    }
+
+
+def main(data: dict, context: dict = None):
+    service = "Data Pipeline - HiBob Time Off"
+    config = load_config(project_id, service)
+    df = get_employees(config)
+    write_to_bigquery(config, df, "WRITE_TRUNCATE")
+
+
+def get_employees(config: dict):
+    cols_to_keep = [
+        "email",
+        "id",
+        "displayName",
+        "managerId",
+        "secondLevelManagerId",
+        "contract",
+    ]
+    url = "https://api.hibob.com/v1/profiles"
+    response = requests.get(url, headers=config["headers"]).json()
+    df = pd.DataFrame(response["employees"])
+    df["managerId"] = df["work"].apply(lambda x: x.get("manager"))
+    df["secondLevelManagerId"] = df["work"].apply(lambda x: x.get("secondLevelManager"))
+    df["contract"] = df["work"].apply(lambda x: x.get("site"))
+    return df[cols_to_keep]
+
+
+if __name__ == "__main__":
+    main({})
--- terraform/environments/infra/functions_hibob.tf
+++ terraform/environments/infra/functions_hibob.tf
@@ -42,3 +42,42 @@ resource "google_cloudfunctions_function" "hibob_time_off" {
 
   }
 }
+
+
+# --------------------------employees--------------------------------\
+data "archive_file" "hibob_employees" {
+  type        = "zip"
+  source_dir  = "../../../cloud_functions/hibob/employees"
+  output_path = "/tmp/hibob_employees.zip"
+}
+
+resource "google_storage_bucket_object" "hibob_employees" {
+  source       = data.archive_file.hibob_employees.output_path
+  content_type = "application/zip"
+  name         = "cloud_function-${data.archive_file.hibob_employees.output_md5}.zip"
+  bucket       = data.google_storage_bucket.function_bucket.name
+}
+
+resource "google_cloudfunctions_function" "hibob_employees" {
+  name                  = "hibob_employees_pipe"
+  runtime               = "python310"
+  available_memory_mb   = 1024
+  timeout               = 540
+  source_archive_bucket = data.google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.hibob_employees.name
+
+  entry_point                  = "main"
+  https_trigger_security_level = "SECURE_ALWAYS"
+  event_trigger {
+    event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
+    resource   = google_pubsub_topic.cloud_function_trigger_hot.id
+  }
+
+  environment_variables = {
+    "DATASET_ID"           = google_bigquery_dataset.hibob_raw.dataset_id
+    "TABLE_NAME"           = google_bigquery_table.employees.table_id
+    "TABLE_LOCATION"       = google_bigquery_dataset.hibob_raw.location
+    "GOOGLE_CLOUD_PROJECT" = var.project
+
+  }
+}
