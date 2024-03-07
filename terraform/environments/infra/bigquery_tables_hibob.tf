140be5183256f9c01cb5a907afe12d9ae4b8d6dc
Balazs Roman <balazs.roman@tpximpact.com>
2024-03-07 16:03:16 +0000

    added employees pipe

--- terraform/environments/infra/bigquery_tables_hibob.tf
+++ terraform/environments/infra/bigquery_tables_hibob.tf
@@ -17,3 +17,22 @@ resource "google_bigquery_table" "time_off" {
   }
 }
 
+resource "google_bigquery_table" "employees" {
+  dataset_id = google_bigquery_dataset.hibob_raw.dataset_id
+  table_id   = "employees"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = var.env
+  }
+
+  deletion_protection = false
+
+  encryption_configuration {
+    kms_key_name = google_kms_crypto_key.bigquery_key.id
+  }
+}
+
