93bc47e8bdc12fb426d702764149a5927d859443
Balazs Roman <balazs.roman@tpximpact.com>
2024-03-12 01:06:18 +0000

    poetry lockfile updates

--- poetry-requirements.sh
+++ poetry-requirements.sh
@@ -4,6 +4,7 @@ find . -not -path '*/\.*' -type f -name "*.toml" | while read toml_file; do
   dir=$(dirname "$toml_file")
   cd "$dir" || exit
   echo "Exporting requirements.txt in $dir"
+  poetry lock --no-update
   poetry export --format=requirements.txt --output=requirements.txt --without-hashes
   sed -i '' -E 's/@[0-9a-f]{40}//g' requirements.txt
   cd - > /dev/null
